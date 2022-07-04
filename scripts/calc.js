60e564578f559d9af5ee949d5794cb7d49ec2ed5
Colm1711 <cosullivan1711@gmail.com>
2022-07-04 20:01:54 +0000

    enough code to make it pass RGR

---
+++ scripts/calc.js
@@ -0,0 +1,8 @@
+// remember the red, green, refactor method. |Only write enough code to get the test passing
+
+function additoin(){
+    return 42;
+}
+
+//need to make sure to export the function so it can be tested.
+module.exports = additoin
