16d97c9b59b350a7a5f9937216d35e8dd7bada9b
Colm1711 <cosullivan1711@gmail.com>
2022-07-04 20:10:53 +0000

    enough code to get a pass for both but not RITE

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,7 +1,7 @@
 // remember the red, green, refactor method. |Only write enough code to get the test passing
-
-function additoin(){
-    return 42;
+// but this is not a RITE tested function.
+function additoin(num1, num2){
+    return num1 + num2;
 }
 
 //need to make sure to export the function so it can be tested.
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,5 +1,6 @@
 //this will be used to test the calc.js file
 //two dots are to signify that file is in seperate folder
+//Please note this is not a RITE test suite as it doesn't test for null params, no params, strings or floats.
 
 const addition = require('../calc')
 
@@ -11,6 +12,9 @@ describe("Addition function", ()=>{
     test("should return 42 for 20 + 22", ()=>{
         //inside the test you must outline your expectations
         expect(addition(20,22)).toBe(42);
+    });
+    test("should return 73 for 42 + 31", ()=>{
+        expect(addition(42,31)).toBe(73);
     });
 });
 describe("Subtraction function", ()=>{
